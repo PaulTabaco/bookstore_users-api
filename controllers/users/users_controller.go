d0681a237675ccdffbfcc0f7f1f28db013c44fde
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-23 18:40:11 +0200

    - CreateUser done in user_controller - User model done - CreateUser done at Services (without real db access)

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,45 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/PaulTabaco/bookstore_users-api/domain/users"
+	"github.com/PaulTabaco/bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implementing of CreateUser coming soon")
+	var user users.User
+	// fmt.Println(user)
+	// bytes, err := ioutil.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	fmt.Println("Read error", err)
+	// 	//TODO: - Handle error
+	// 	return
+	// }
+
+	// if err = json.Unmarshal(bytes, &user); err != nil {
+	// 	fmt.Println("Unmarshal err", err, "Bytes - ", string(bytes))
+	// 	//TODO: - Handle error
+	// 	return
+	// }
+
+	/// !! next ..ShouldBindJSON.. do same as commented code above
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println("ShouldBindJSON", err)
+		//TODO: - Handle json error
+		return
+	}
+
+	result, err := services.CreateUser(user)
+
+	if err != nil {
+		//TODO: - Handle error
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
