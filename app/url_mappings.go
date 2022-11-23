86e2632fa2d221527288356eb94258bf940f23e2
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-24 00:38:19 +0200

    Fix url mapping

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -8,7 +8,7 @@ import (
 func MapUrls() {
 	router.GET("/ping", ping.Ping)
 
-	router.GET("/users/:user_id", users.CreateUser)
+	router.GET("/users/:user_id", users.GetUser)
 	router.GET("/users/search", users.SearchUser)
 	router.POST("/users", users.CreateUser)
 }
