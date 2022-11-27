a5bbe9099f7b585931d1055bd1966e059f5be844
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-27 22:47:21 +0200

    - UpdateUser with PUT logic added (updating all fields with filled and empty fields); - UpdateUser with PATCH logic added (touching only filled fields);

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -45,6 +45,35 @@ func GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, user)
 }
 
+func UpdateUser(c *gin.Context) {
+	userId, userIdErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userIdErr != nil {
+		err := errors.NewBadRequestError("invalide user id")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, &restErr)
+		return
+	}
+
+	user.Id = userId
+
+	isPartial := c.Request.Method == http.MethodPatch
+
+	result, err := services.UpdateUser(isPartial, user)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
 func SearchUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implementing of SearchUser coming soon")
 }
--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -8,15 +8,19 @@ import (
 
 type User struct {
 	Id          int64  `json:"id"`
-	FirstName   string `json:"first_name" binding:"required"`
-	LastName    string `json:"last_name"  binding:"required"`
-	Email       string `json:"email" binding:"required,email"`
+	FirstName   string `json:"first_name"` // `json:"first_name" binding:"required"`
+	LastName    string `json:"last_name"`  // `json:"last_name" binding:"required"`
+	Email       string `json:"email"`      // `json:"email" binding:"required,email"`
 	DateCreated string `json:"date_created"`
 }
 
 func (user *User) Validate() *errors.RestErr {
-	// Just for tutorial flow, but for better way I used - binding: in json tags
-	if strings.TrimSpace(strings.ToLower(user.Email)) == "" {
+
+	user.FirstName = strings.TrimSpace(user.FirstName)
+	user.LastName = strings.TrimSpace(user.LastName)
+
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
 		return errors.NewBadRequestError("invalid email address")
 	}
 	return nil
