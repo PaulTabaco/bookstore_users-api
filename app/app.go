2330f0d4be96dbaaab94cf5421898f105d1cf1da
Paul Tabaco <rabochiy.delta@gmail.com>
2022-11-30 20:14:51 +0200

    - got zap lib for logging; - logger package added with init config; - start logger in StartApp func;

--- app/app.go
+++ app/app.go
@@ -1,10 +1,14 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/PaulTabaco/bookstore_users-api/logger"
+	"github.com/gin-gonic/gin"
+)
 
 var router = gin.Default()
 
 func StartApp() {
 	MapUrls()
+	logger.Log.Info("about to start the application")
 	router.Run(":8080")
 }
