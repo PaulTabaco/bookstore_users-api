a32328b3fea54ae5bb7bcfe3d615cf5288c14151
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-10 14:03:52 +0200

    - 8081 port used to start bookstore_users-API;

--- app/app.go
+++ app/app.go
@@ -11,5 +11,5 @@ func StartApp() {
 	MapUrls()
 	// logger.Log.Info("about to start the application") // Info in logger.Log
 	logger.Info("about to start the application") // Customized by me in logger with Sync()
-	router.Run(":8080")
+	router.Run(":8081")
 }
